32ba990133f24c66dfe4a149d339f21e14bae0d4
Uriah Glen Twohill <uriah.twohill@soit.local>
2019-08-12 14:52:50 +1200

    First commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform hero;
+    
+    void FixedUpdate()
+    {
+        if (hero.position.x >= 10)
+        {
+            transform.position = new Vector3(hero.position.x, transform.position.y, transform.position.z);
+        }
+    }
+    }
+    
+
---
+++ Assets/Scripts/HeroScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroScript : MonoBehaviour {
+
+    public float jumpForce = 7.0f;
+    private bool onGround = false;
+    public int movementSpeed = 4;
+    private Animator anim;
+ 
+    Vector2 originalPosH;
+    public GameObject hero;
+    
+
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("up"))
+        {
+            if (onGround == true)
+            {
+                Rigidbody2D rb = GetComponent<Rigidbody2D>();
+                rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                onGround = false;
+                anim.SetTrigger("Jump");
+                print("Hero has jumped");
+            }
+        }
+        if (onGround == true)
+        {
+            Rigidbody2D rb1 = GetComponent<Rigidbody2D>();
+            rb1.velocity = new Vector2(movementSpeed, 0);
+        }        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onGround = true;
+        print("Hero has touched ground");
+    }
+}
