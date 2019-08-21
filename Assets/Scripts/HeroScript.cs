5d899fcab103bebbffe4137e947ff1ec69fe7c00
Uriah Glen Twohill <uriah.twohill@soit.local>
2019-08-21 20:30:22 +1200

    Not many new features but code is far cleaner

--- Assets/Scripts/HeroScript.cs
+++ Assets/Scripts/HeroScript.cs
@@ -7,10 +7,9 @@ public class HeroScript : MonoBehaviour {
 
 
     //Hero declarations
-    public float jumpForce = 7.0f;
+    public float jumpForce = 10.0f;
     private bool onGround = false;
-    public int movementSpeedRight = 4;
-    public int movementSpeedLeft = 4;
+    public int movementSpeed = 5;
     private Animator anim;
     public float health = 100;
     public Text healthText;
@@ -28,19 +27,33 @@ public class HeroScript : MonoBehaviour {
 
 
 
-    // Use this for initialization
+
     void Start () {
         anim = GetComponent<Animator>();
     }
 	
-	// Update is called once per frame
-	void Update () {
+     
+    // Contains all movement code and user controlled animations.
+	void Update ()
+    {
+        // Standing still animation.
         if (Input.anyKey == false && onGround == true)
         {
             anim.SetInteger("Transition", 1);
-            DisplayLives();
         }
 
+        Jump();
+        Running();
+        DisplayHealthAndLives();
+     }
+
+        //----------------------------------------------------------------------------------------------------------------------------//
+       //----------------------------------------------------------Movement----------------------------------------------------------//
+      //----------------------------------------------------------------------------------------------------------------------------//
+
+    // Jumping
+    public void Jump()
+    {
         if (Input.GetKeyDown("up"))
         {
             if (onGround == true)
@@ -52,7 +65,10 @@ public class HeroScript : MonoBehaviour {
                 print("Hero has jumped");
             }
         }
-
+    }
+    // Running
+    public void Running()
+    {
         if (Input.GetKey("right") && onGround == true)
         {
             if (Input.GetKey("right") && onGround == true && heroScaleCounter == 0)
@@ -61,7 +77,7 @@ public class HeroScript : MonoBehaviour {
                 heroScaleCounter = 1;
             }
             Rigidbody2D rb1 = GetComponent<Rigidbody2D>();
-            rb1.velocity = new Vector2(movementSpeedRight, 0);
+            rb1.velocity = new Vector2(movementSpeed, 0);
             anim.SetInteger("Transition", 2);
         }
 
@@ -73,53 +89,88 @@ public class HeroScript : MonoBehaviour {
                 heroScaleCounter = 0;
             }
             Rigidbody2D rb1 = GetComponent<Rigidbody2D>();
-            rb1.velocity = new Vector2(-movementSpeedLeft, 0);
+            rb1.velocity = new Vector2(-movementSpeed, 0);
             anim.SetInteger("Transition", 2);
         }
-        DisplayHealth();
     }
 
+        //------------------------------------------------------------------------------------------------------------------------------//
+       //------------------------------------------------------Passive Mechanics-------------------------------------------------------//
+      //------------------------------------------------------------------------------------------------------------------------------//
+
+    // Ground detection.
     private void OnCollisionEnter2D(Collision2D collision)
     {
         onGround = true;
         print("Hero has touched ground");
     }
 
-    public void DisplayLives()
-    {
-        livesText.text = "lives: " + lives;
-    }
-
-    public void DisplayHealth()
+    // Display Health and Lives function.
+    public void DisplayHealthAndLives()
     {
         if (health >= 0)
         {
             healthText.text = "H: " + health;
+            livesText.text = "lives: " + lives;
         }
         else
         {
-            anim.SetInteger("Transition", 4);
-            Invoke("Respawn", 3);
-            
-
-            if (lives >= 1)
-            {
-                lives -= 1;
-            }
-           
-            
-           // anim.SetInteger("Transition", 5);
-            
+            HeroDies();
+            ReduceLife();     
         }
     }
 
-   public void Respawn()
+        //------------------------------------------------------------------------------------------------------------------------------//
+       //-----------------------------------------------------------Death--------------------------------------------------------------//
+      //------------------------------------------------------------------------------------------------------------------------------//
+
+    // The Hero Dies.
+    public void HeroDies()
     {
-        health = 100;
-        transform.position = new Vector3(camera.position.x -5, transform.position.y, transform.position.z);
+        anim.SetInteger("Transition", 4);
+        FreezeMovement();
+        
+     //   if (lives >= 1)
+     //   {
+            Invoke("Respawn", 1.5f);
+    //    }
+        /*
+        else ()
+        {
+            Game over transition to be coded.
+        }
+        */
+    }
+
+    // Reduces life
+    public void ReduceLife()
+    {
+        if (lives >= 1)
+        {
+            lives -= 1;
+        }
+    }
+
+    // Respawns Hero and resets health to 100.
+    public void Respawn()
+    {
         anim.SetInteger("Transition", 5);
-        Invoke("anim.SetInteger('Transition'", 2);
+        transform.position = new Vector3(camera.position.x - 10, transform.position.y, transform.position.z);
+        health = 100;
+        UnFreezeMovement();
     }
 
 
+    // Freezing and UnFreezing movement.
+    public void FreezeMovement()
+    {
+        onGround = false;
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+    }
+
+    public void UnFreezeMovement()
+    {
+        onGround = true;
+        gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None | RigidbodyConstraints2D.None | RigidbodyConstraints2D.FreezeRotation;
+    }
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,33 +6,24 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
 
-
-
     public Text scoreText;
     private float score;
     private float startTime;
-    //public Text livesText;
-    //public float lives = 3;
+    
     public float jumpCounter;
 
-    public GameObject hero;
 
-    HeroScript heroScript;
-    
-
-    // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         ScoreIncrease();
-      //  DisplayLives();
-        
+       
     }
+
     public void IncreaseOnJump()
     {
         jumpCounter += 2;
@@ -41,15 +32,7 @@ public class ScoreManager : MonoBehaviour
     public void ScoreIncrease()
     {
         score = (int)(Time.time - startTime);
+        score = score + jumpCounter;
         scoreText.text = "S: " + score;
     }
-
-   /* public void DisplayLives()
-    {
-        livesText.text = "lives: " + lives;
-        
-    }
-    */
-    
-
 }
